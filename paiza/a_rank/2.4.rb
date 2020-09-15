ea0e9a73745f3f5a22bc30f17168221993e57134
K80Git <67084301+K80Git@users.noreply.github.com>
2020-09-16 08:19:56 +0900

    paiza test

--- paiza/a_rank/2.4.rb
+++ paiza/a_rank/2.4.rb
@@ -1,9 +1,122 @@
 =begin
 問題
+下記の問題をプログラミングしてみよう！
+開始時点の x , y 座標と移動の歩数 N が与えられます。
+以下の図のように時計回りに渦を巻くように移動を N 歩行った後の x , y 座標 を答えてください。
+なお、マスの座標系は下方向が y 座標の正の向き、右方向が x 座標の正の向きとします。
 
+
+
+▼　下記解答欄にコードを記入してみよう
+
+入力される値
+X Y N
+
+
+・ 1 行で、開始時点の x , y 座標を表す X , Y, 移動の歩数 N が与えられます。
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+1行での出力
+・ 移動を N 歩行った後の x , y 座標を出力してください。
+
+
+x y
+条件
+すべてのテストケースにおいて、以下の条件を満たします。
+・ -100 ≦ X, Y ≦ 100
+・ 0 ≦ N ≦ 100
+
+入力例1
+0 0 3
+
+出力例1
+0 1
+
+入力例2
+38 47 27
+
+出力例2
+41 47
 
 =end
 puts "回答--------------"
+
+direc = gets.chomp.split(" ")
+xy = [direc[0].to_i, direc[1].to_i]
+count = direc[2].to_i
+
+if count >= 72 && count<= 81
+  xy[1] -= 4
+end
+
+if (count>= 42&&count<= 49)|| count == 71 || count ==82
+  xy[1] -= 3
+end
+
+if (count >= 20&& count <= 25)|| count== 50 || count ==83
+  xy[1] -= 2 
+end
+
+if (count >= 6&&count <= 9)|| count ==69|| count ==40|| count ==19|| count ==26|| count ==51|| count ==84
+  xy[1] -= 1
+end
+
+if (count >= 2&& count<=4)|| count ==67|| count ==38|| count ==17|| count ==11|| count ==28|| count ==53|| count ==86
+  xy[1] +=1
+end
+
+if (count>=12 &&count <=16)|| count ==66 || count ==37|| count ==29|| count ==54|| count ==87
+  xy[1] += 2
+end
+
+if (count >=30 &&count<=36)|| count ==65 || count ==55|| count ==88
+  xy[1] += 3
+end
+
+if (count >= 56 && count<= 64)|| count ==89
+  xy[1] += 4
+end
+
+if count >=90
+  xy[1] += 5
+end
+
+if (count>= 81 && count<= 90 ) 
+  xy[0] += 5
+end
+
+if (count>= 49 && count<= 56 )|| count == 80 || count == 91
+  xy[0] += 4
+end
+if (count>= 25 && count<= 30 )|| count == 79 || count == 48 || count == 57 || count == 92 
+  xy[0] += 3
+end
+if (count>= 9 && count<= 12 )|| count == 78 || count == 47 || count == 24 || count == 31 || count ==58|| count ==93
+  xy[0] += 2
+end
+if (count>= 1 && count<= 2 )|| count == 77 || count == 46 || count == 23 || count == 8 || count ==13|| count ==32|| count ==59|| count ==94
+  xy[0] += 1
+end
+if (count>= 4 && count<= 6 )|| count == 75 || count == 44 || count == 21 || count == 15 || count ==34|| count ==61|| count ==96
+  xy[0] -=1
+end
+if (count>= 16 && count<= 20 )|| count == 74 || count == 43 || count == 62 || count == 97 
+  xy[0] -= 2
+end
+
+if (count>=36  && count<= 42 )|| count == 73 || count == 63 || count == 98
+  xy[0] -=3
+end
+if (count>= 64 && count<= 72 )|| count == 99
+  xy[0] -= 4
+end
+if count == 100
+  xy[0] -=5
+end
+
+puts "#{xy[0]} #{xy[1]}"
 
 
 puts "解答--------------"
--- paiza/a_rank/2.5.rb
+++ paiza/a_rank/2.5.rb
@@ -1,9 +1,183 @@
 =begin
 問題
+開始時点の x , y 座標,移動の回数 N が与えられます。
+続くN行で移動の向き d1 ... dN が与えられるので、与えられた順に移動をしたときの各移動後の x , y 座標 を答えてください。
+移動者ははじめ北を向いています。
+なお、マスの座標系は下方向が y 座標の正の向き、右方向が x 座標の正の向きとします。
+移動をするごとに向く方角が変わること・移動前に向いている方角によって同じ移動の向きでも座標の変化が違うことに気をつけてください。
+例えば、上の図の状態から右に移動を行った場合、下の図のような状態になります。
+
+
+
+
+▼　下記解答欄にコードを記入してみよう
+
+入力される値
+X Y N       
+d1      
+...     
+dN
+
+
+・ 1 行目には、開始時点の x , y 座標を表す X , Y, 移動の回数 N が与えられます。
+・ 続く N 行 (1 ≦ i ≦ N) には、盤面の i 回目の移動の向きを表す文字 d_i が与えられます。
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+N 行での出力
+・ 各移動後の x , y 座標を出力してください。
+
+
+x_1 y_1
+...
+x_N y_N
+条件
+すべてのテストケースにおいて、以下の条件をみたします。
+・ -100 ≦ X, Y ≦ 100
+・ 1 ≦ N ≦ 100
+・ d は、L, R のいずれかでそれぞれ 左・右 に １ マス進むことを表す。
+
+入力例1
+3 5 1
+L
+
+出力例1
+2 5
+
+入力例2
+-18 45 6
+L
+L
+R
+R
+L
+R
+
+出力例2
+-19 45
+-19 46
+-20 46
+-20 45
+-21 45
+-21 44
+
 
 
 =end
 puts "回答--------------"
+
+str = gets.chomp.split(" ")
+str_x = str[0].to_i
+str_y = str[1].to_i
+count = str[2].to_i
+xy = [str_x,str_y]
+
+
+
+dirs_array =[]
+(1..count).each do
+  rl = gets.chomp
+  dirs_array.push(rl)
+end
+
+def east(rl,xy,hogaku)   # rl=E
+  if rl == "R"
+    xy[1]+= 1
+    puts "#{xy[0]} #{xy[1]}"
+  else
+    xy[1] -= 1
+    puts "#{xy[0]} #{xy[1]}"
+  end
+end
+def east_hoga(rl,hogaku)
+  if rl == "R"
+    hogaku += 90
+    puts hogaku
+  else
+    hogaku-= 90
+    puts hogaku
+  end
+end
+
+def west(rl,xy,hogaku)  # rl=W
+  if rl == "R"
+    xy[1] -= 1
+    puts "#{xy[0]} #{xy[1]}"
+  else
+    xy[1] += 1
+    puts "#{xy[0]} #{xy[1]}"
+  end
+end
+
+def west_hoga(rl,hogaku)
+  if rl == "R"
+    hogaku += 90
+    puts hogaku
+  else
+    hogaku -= 90
+    puts hogaku
+  end
+end
+
+def south(rl,xy,hogaku) # rl=S
+  if rl == "R"
+    xy[0] -= 1
+    puts "#{xy[0]} #{xy[1]}"
+  else
+    xy[0] += 1
+    puts "#{xy[0]} #{xy[1]}"
+  end
+end
+def south_hoga(rl,hogaku)
+  if rl == "R"
+    hogaku += 90
+    puts hogaku
+  else
+    hogaku -= 90
+    puts hogaku
+  end
+end
+
+def north(rl,xy,hogaku) # rl=N
+  if rl == "R"
+    xy[0] += 1
+    puts "#{xy[0]} #{xy[1]}"
+  else
+    xy[0] -= 1
+    puts "#{xy[0]} #{xy[1]}"
+  end
+end
+def north_hoga(rl,hogaku)
+  if rl == "R"
+    hogaku = hogaku + 90
+  else
+    hogaku = hogaku - 90
+  end
+end
+
+dirs_array.each do |rl|
+  hogaku = 0
+  print "初期値：#{hogaku}\n"
+  if hogaku == 90 || hogaku == -270
+    east(rl,xy,hogaku)
+    east_hoga(rl,hogaku)
+  elsif hogaku == 180 || hogaku == -180
+    west(rl,xy,hogaku)
+    west_hoga(rl,hogaku)
+  elsif hogaku == 270 || hogaku == -90
+    south(rl,xy,hogaku)
+    south_hoga(rl,hogaku)
+  else
+    north(rl,xy,hogaku)
+    north_hoga(rl,hogaku)
+  end
+
+
+  print "最終値：#{hogaku}\n"
+
+end
+
 
 
 puts "解答--------------"
